ec51e2ec92a6012726e4f68ae80a2f3d9363f26d
Victor Gesuato da Silva <victor.gesuato@ebox.ca>
2022-05-17 12:15:14 -0400

    setup Hilt

--- app/src/main/java/com/example/jetpackcomposemvvm/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposemvvm/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.jetpackcomposemvvm
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
+class JetpackComposeMvvmApp : Application()
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
